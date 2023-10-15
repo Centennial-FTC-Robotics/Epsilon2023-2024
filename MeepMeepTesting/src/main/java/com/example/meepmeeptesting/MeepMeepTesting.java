4d18854bac5ad5f23d2e35ec7497c98eb264b165
pi-person314 <jadenmengl@gmail.com>
2023-10-15 14:42:19 -0400

    testing meepmeep auto path

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -19,15 +20,20 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(62, -34, Math.toRadians(180)))
+                                .forward(24)
+                                .turn(Math.toRadians(-90))
+                                .forward(78)
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                .strafeTo(new Vector2d(60,50))
+                                .forward(10)
                                 .build()
                 );
 
