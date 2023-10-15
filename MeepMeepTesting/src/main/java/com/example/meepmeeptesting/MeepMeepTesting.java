37a89fefdc178f2f867e6b80253df8ec1eda6d9c
aryans0714 <aryan.sharma0714@gmail.com>
2023-10-15 15:17:37 -0400

    terrible meep meep work this is so scuffed

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -16,30 +16,123 @@ public class MeepMeepTesting {
 
     public static RoadRunnerBotEntity bottomRed(MeepMeep meepMeep) {
         return new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(62, -34, Math.toRadians(180)))
                                 .forward(24)
+                                // place purple pixel
                                 .turn(Math.toRadians(-90))
                                 .forward(78)
+                                // place yellow pixel
                                 .turn(Math.toRadians(180))
                                 .forward(98)
+                                // pickup white pixel
                                 .turn(Math.toRadians(180))
                                 .forward(98)
+                                // place white pixel
                                 .turn(Math.toRadians(180))
                                 .forward(98)
+                                // pickup white pixel
                                 .turn(Math.toRadians(180))
                                 .forward(98)
+                                // place white pixel
                                 .strafeTo(new Vector2d(60,50))
                                 .forward(10)
                                 .build()
                 );
     }
+
+    public static RoadRunnerBotEntity topRed(MeepMeep meepMeep) {
+        return new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(62, 12, Math.toRadians(180)))
+                                .forward(24)
+                                // place purple pixel
+                                .turn(Math.toRadians(-90))
+                                .forward(32)
+                                // place yellow pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // pickup white pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // place white pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // pickup white pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // place white pixel
+                                .strafeTo(new Vector2d(60,50))
+                                .forward(10)
+                                .build()
+                );
+    }
+
+    public static RoadRunnerBotEntity bottomBlue(MeepMeep meepMeep) {
+        return new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-62, -34, Math.toRadians(0)))
+                                .forward(24)
+                                // place purple pixel
+                                .turn(Math.toRadians(90))
+                                .forward(78)
+                                // place yellow pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // pickup white pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // place white pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // pickup white pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // place white pixel
+                                .strafeTo(new Vector2d(-60,50))
+                                .forward(10)
+                                .build()
+                );
+    }
+
+    public static RoadRunnerBotEntity topBlue(MeepMeep meepMeep) {
+        return new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-62, 12, Math.toRadians(0)))
+                                .forward(24)
+                                // place purple pixel
+                                .turn(Math.toRadians(90))
+                                .forward(32)
+                                // place yellow pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // pickup white pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // place white pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // pickup white pixel
+                                .turn(Math.toRadians(180))
+                                .forward(98)
+                                // place white pixel
+                                .strafeTo(new Vector2d(-60,50))
+                                .forward(10)
+                                .build()
+                );
+    }
+
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = bottomRed(meepMeep);
+        // RoadRunnerBotEntity myBot = topBlue(meepMeep);
+        // RoadRunnerBotEntity myBot = bottomBlue(meepMeep);
+        // RoadRunnerBotEntity myBot = topRed(meepMeep);
+        // RoadRunnerBotEntity myBot = bottomRed(meepMeep);
 
         Image img = null;
         try { img = ImageIO.read(new File("MeepMeepTesting\\src\\main\\java\\com\\example\\meepmeeptesting\\background.png")); }
