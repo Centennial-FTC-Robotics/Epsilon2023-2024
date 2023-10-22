55569870cc83439fcc254d3b3a3fc64874387abc
aryans0714 <aryan.sharma0714@gmail.com>
2023-10-22 14:49:50 -0400

    LETS Goooooooooooo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagAutonomousInitDetectionExample.java
@@ -45,10 +45,10 @@ public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
     // UNITS ARE PIXELS
     // NOTE: this calibration is for the C920 webcam at 800x448.
     // You will need to do your own calibration for other configurations!
-    double fx = 578.272;
-    double fy = 578.272;
-    double cx = 402.145;
-    double cy = 221.506;
+    double fx = 0;
+    double fy = 0;
+    double cx = 0;
+    double cy = 0;
 
     // UNITS ARE METERS
     double tagsize = 0.166;
