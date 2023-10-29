a2c041c0960514f54f19fb1f323e127e8b028467
pi-person314 <jadenmengl@gmail.com>
2023-10-29 12:02:39 -0400

    added a few subsystems with ftc lib

---
+++ TeamCode/src/main/java/Subsystems/Intake.java
@@ -0,0 +1,10 @@
+package Subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake extends SubsystemBase {
+    public Intake(final HardwareMap hMap, final String name) {
+
+    }
+}
---
+++ TeamCode/src/main/java/Subsystems/Outtake.java
@@ -0,0 +1,10 @@
+package Subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Outtake extends SubsystemBase {
+    public Outtake(final HardwareMap hMap, final String name) {
+
+    }
+}
