2fefeeede1e0169b8c88b9a636105687dae4c919
aryans0714 <aryan.sharma0714@gmail.com>
2023-12-05 20:40:52 -0500

    pls work pls work pls work pls work PLEASE WORK I HAVE A WORLD TEST TO STUDY FOR

---
+++ TeamCode/src/main/java/Epsilon/Subsystems/BlueCubeDetectionPipeline.java
@@ -0,0 +1,68 @@
+//this will work maybe!!
+package Epsilon.Subsystems;
+
+import org.opencv.core.MatOfPoint;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+
+public class BlueCubeDetectionPipeline extends OpenCvPipeline {
+    private Mat hsvMat = new Mat();
+    private Mat maskMat = new Mat();
+    private ArrayList<MatOfPoint> contourMat = new ArrayList<>();
+
+    private double cubeCenterX = -1;
+    private double cubeCenterY = -1;
+    private boolean cubeDetected = false;
+
+    private final double cubeSize;
+
+    public BlueCubeDetectionPipeline(double cubeSize) {
+        this.cubeSize = cubeSize;
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
+        Scalar lowerBlue = new Scalar(100, 100, 100); //can change later if needed ig
+        Scalar upperBlue = new Scalar(130, 255, 255); //can change later if needed ig
+        Core.inRange(hsvMat, lowerBlue, upperBlue, maskMat);
+        Imgproc.findContours(maskMat, contourMat, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        cubeCenterX = -1;
+        cubeCenterY = -1;
+        cubeDetected = false;
+
+        if (!contourMat.isEmpty()) {
+            for (int i = 0; i < contourMat.size(); i++) {
+                Rect rect = Imgproc.boundingRect(contourMat.get(i));
+                double contourArea = Imgproc.contourArea(contourMat.get(i));
+                if (Math.abs(1 - (rect.width / rect.height)) < 0.2 && contourArea > 1000) {
+                    cubeCenterX = rect.x + rect.width / 2.0;
+                    cubeCenterY = rect.y + rect.height / 2.0;
+                    cubeDetected = true;
+                    Imgproc.rectangle(input, rect.tl(), rect.br(), new Scalar(0, 255, 0), 2);
+                    break;
+                }
+            }
+        }
+
+        return input;
+    }
+
+    public double getCubeCenterX() {
+        return cubeCenterX;
+    }
+
+    public double getCubeCenterY() {
+        return cubeCenterY;
+    }
+
+    public boolean isCubeDetected() {
+        return cubeDetected;
+    }
+}
