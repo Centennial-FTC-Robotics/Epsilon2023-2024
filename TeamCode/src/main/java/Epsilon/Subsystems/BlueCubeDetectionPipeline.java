28a4c9c5d2fbfc0373b3f7142d23cde7e270d214
aryans0714 <aryan.sharma0714@gmail.com>
2023-12-08 08:10:20 -0500

    vision working on one test case lets go

--- TeamCode/src/main/java/Epsilon/Subsystems/BlueCubeDetectionPipeline.java
+++ TeamCode/src/main/java/Epsilon/Subsystems/BlueCubeDetectionPipeline.java
@@ -1,4 +1,3 @@
-//this will work maybe!!
 package Epsilon.Subsystems;
 
 import org.opencv.core.MatOfPoint;
@@ -8,6 +7,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import java.util.ArrayList;
 
@@ -20,33 +20,57 @@ public class BlueCubeDetectionPipeline extends OpenCvPipeline {
     private double cubeCenterY = -1;
     private boolean cubeDetected = false;
 
-    private final double cubeSize;
+    private MatOfPoint largestContour = null;
+    private Rect largestRect = null;
 
-    public BlueCubeDetectionPipeline(double cubeSize) {
-        this.cubeSize = cubeSize;
+    Telemetry telemetry;
+
+    public static Scalar lowerBlue = new Scalar(100, 75, 0);
+    public static Scalar upperBlue = new Scalar(150, 150, 250);
+
+    public BlueCubeDetectionPipeline(Telemetry telemetry) {
+        this.telemetry = telemetry;
     }
 
     @Override
     public Mat processFrame(Mat input) {
         Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
-        Scalar lowerBlue = new Scalar(120, 100, 255); //can change later if needed ig
-        Scalar upperBlue = new Scalar(160, 185, 255); //can change later if needed ig
         Core.inRange(hsvMat, lowerBlue, upperBlue, maskMat);
         Imgproc.findContours(maskMat, contourMat, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         cubeCenterX = -1;
         cubeCenterY = -1;
         cubeDetected = false;
 
-        if (!contourMat.isEmpty()) {
-            for (int i = 0; i < contourMat.size(); i++) {
-                Rect rect = Imgproc.boundingRect(contourMat.get(i));
-                //double contourArea = Imgproc.contourArea(contourMat.get(i));
-                cubeCenterX = rect.x + rect.width / 2.0;
-                cubeCenterY = rect.y + rect.height / 2.0;
-                cubeDetected = true;
-                Imgproc.rectangle(input, rect.tl(), rect.br(), new Scalar(0, 255, 0), 2);
-                break;
+        int MIN_CONTOUR_AREA_THRESHOLD = 0;
+
+        telemetry.addLine(Integer.toString(contourMat.size()));
+        largestContour = null;
+        largestRect = null;
+
+        for (int i = 0; i < contourMat.size(); i++) {
+            Rect rect = Imgproc.boundingRect(contourMat.get(i));
+
+            double area = Imgproc.contourArea(contourMat.get(i));
+            if (area > MIN_CONTOUR_AREA_THRESHOLD) {
+                if (largestContour == null || area > Imgproc.contourArea(largestContour)) {
+                    largestContour = contourMat.get(i);
+                    largestRect = rect;
+                }
             }
+        }
+
+        if (largestContour != null) {
+            Imgproc.rectangle(input, largestRect.tl(), largestRect.br(), new Scalar(0, 255, 0), 2);
+
+            cubeCenterX = largestRect.x + largestRect.width / 2.0;
+            cubeCenterY = largestRect.y + largestRect.height / 2.0;
+
+            telemetry.addLine("Cube Detected");
+            telemetry.addData("Center X", cubeCenterX);
+            telemetry.addData("Center Y", cubeCenterY);
+            telemetry.update();
+
+            cubeDetected = true;
         }
 
         return input;
