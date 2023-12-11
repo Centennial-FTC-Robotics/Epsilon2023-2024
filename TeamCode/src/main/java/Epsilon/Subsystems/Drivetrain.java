a06a3f0575ecad16e0c59b124206fbf89fb67d4c
Warriorninja245 <114183419+Warriorninja245@users.noreply.github.com>
2023-12-10 21:10:54 -0500

    Made it get better power, for the motors so they go faster

--- TeamCode/src/main/java/Epsilon/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/Epsilon/Subsystems/Drivetrain.java
@@ -89,10 +89,10 @@ public class Drivetrain implements Subsystem {
         double drive = gamepad1.left_stick_y;
         double strafe = -gamepad1.left_stick_x;
         double turn = -gamepad1.right_stick_x;
-        double frontLeftPower = Range.clip(drive + strafe + turn, -0.5, 0.5);
-        double frontRightPower = Range.clip(drive - strafe - turn, -0.5, 0.5);
-        double backLeftPower = Range.clip(drive - strafe + turn, -0.5, 0.5);
-        double backRightPower = Range.clip(drive + strafe - turn, -0.5, 0.5);
+        double frontLeftPower = Range.clip(drive + strafe + turn, -1, 1);
+        double frontRightPower = Range.clip(drive - strafe - turn, -1, 1);
+        double backLeftPower = Range.clip(drive - strafe + turn, -1, 1);
+        double backRightPower = Range.clip(drive + strafe - turn, -1, 1);
 
         frontLeft.setPower(frontLeftPower);
         frontRight.setPower(frontRightPower);
