d46ff329c4a122f692f37dafbb51d8020ebd8c57
pi-person314 <jadenmengl@gmail.com>
2024-01-01 15:38:59 -0500

    back auto pog

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BackRedVisionAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BackRedVisionAuto.java
@@ -20,22 +20,21 @@ public class BackRedVisionAuto extends LinearOpMode {
 
         robot.drivetrain.move(28, "drive");
         if (cubeLocation == ElementProcessor.PropPositions.RIGHT) {
-            robot.drivetrain.move(23, "rotate");
+            robot.drivetrain.move(20.5, "rotate");
             Thread.sleep(1000);
-            robot.drivetrain.move(-23, "rotate");
         } else if (cubeLocation == ElementProcessor.PropPositions.LEFT) {
-            robot.drivetrain.move(-23, "rotate");
+            robot.drivetrain.move(-20.5, "rotate");
             Thread.sleep(1000);
-            robot.drivetrain.move(23, "rotate");
+            robot.drivetrain.move(41, "rotate");
         } else if (cubeLocation == ElementProcessor.PropPositions.MIDDLE) {
             Thread.sleep(1000);
+            robot.drivetrain.move(20.5, "rotate");
         } else {
             telemetry.addLine("cube not found");
             telemetry.update();
         }
-        robot.drivetrain.move(21, "rotate");
-        robot.drivetrain.move(24, "drive");
-        robot.drivetrain.move(26, "strafe");
-        robot.drivetrain.move(24, "drive");
+        robot.drivetrain.move(34, "drive");
+        robot.drivetrain.move(28, "strafe");
+        robot.drivetrain.move(14, "drive");
     }
 }
