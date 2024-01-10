2d4b6fba8d53e437d84433f05173b30d8fb34b87
aryans0714 <aryan.sharma0714@gmail.com>
2024-01-10 16:44:11 -0500

    i hat e ahiet haite ahiet iaeht aiht ihawet ihaewt ihaet iaht

--- TeamCode/src/main/java/Epsilon/OurRobot.java
+++ TeamCode/src/main/java/Epsilon/OurRobot.java
@@ -7,6 +7,7 @@ import Epsilon.Subsystems.IMU;
 import Epsilon.Subsystems.Intake;
 import Epsilon.Subsystems.Outtake;
 import Epsilon.Subsystems.Vision;
+import Epsilon.Subsystems.Launcher;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class OurRobot {
@@ -15,17 +16,20 @@ public class OurRobot {
     public Intake intake;
     public Outtake outtake;
     public Vision vision;
+    public Launcher launcher;
 
     public OurRobot(LinearOpMode opMode) {
         drivetrain = new Drivetrain(opMode.hardwareMap);
         imu = new IMU(opMode.hardwareMap);
         intake = new Intake(opMode.hardwareMap);
+        launcher = new Launcher(opMode.hardwareMap);
         //outtake = new Outtake(opMode.hardwareMap);
     }
 
     public void teleOpUpdate(Gamepad gamepad1, Gamepad gamepad2) throws InterruptedException {
         drivetrain.teleOpUpdate(gamepad1, gamepad2);
         intake.teleOpUpdate(gamepad1, gamepad2);
+        launcher.teleOpUpdate(gamepad1, gamepad2);
         //outtake.teleOpUpdate(gamepad1, gamepad2);
     }
 
---
+++ TeamCode/src/main/java/Epsilon/Subsystems/Launcher.java
@@ -0,0 +1,21 @@
+package Epsilon.Subsystems;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import Epsilon.Subsystem;
+
+public class Launcher implements Subsystem {
+    public Servo launcher;
+    public Launcher(final HardwareMap hMap) {
+        launcher = hMap.get(Servo.class, "launcherServo");
+    }
+
+    public void launch() {
+        launcher.setPosition(0.5); //?@??@?@?@?@????@?@
+    }
+
+    public void teleOpUpdate(Gamepad gamepad1, Gamepad gamepad2) {
+        if (gamepad2.y) launch();
+    }
+}
